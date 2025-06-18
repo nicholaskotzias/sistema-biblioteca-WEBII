1c2b9c653b83d9f9c3dcc5326bb46bc454b61efb
nicholaskotzias <nicokotzias@gmail.com>
2025-06-18 09:37:34 -0300

    feat: adicionar model Aluno com SoftDeletes e relacionamento com User

---
+++ projeto-sistema-biblioteca/app/Models/Aluno.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Aluno extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'matricula',
+        'curso',
+        'data_nascimento',
+    ];
+
+    protected $casts = [
+        'data_nascimento' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
