3e580d760329ecb76c9b84c3cfbf46a8a1f9e008
nicholaskotzias <nicokotzias@gmail.com>
2025-06-19 22:49:50 -0300

    feat: adicionar Controllers de Autor, Livro, Categoria, Exemplar e Emprestimo com RESTful vazios

---
+++ projeto-sistema-biblioteca/app/Http/Controllers/AutorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Autor;
+use Illuminate\Http\Request;
+
+class AutorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Autor $autor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Autor $autor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Autor $autor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Autor $autor)
+    {
+        //
+    }
+}
