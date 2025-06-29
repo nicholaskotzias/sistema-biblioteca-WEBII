19d48f16050847f1e8ebd41d9c035c2dd604713b
nicholaskotzias <nicokotzias@gmail.com>
2025-06-29 20:59:59 -0300

    chore: adicionar redirecionamento ao criar aluno no controller de admin: aluno

--- projeto-sistema-biblioteca/app/Http/Controllers/Admin/AlunoController.php
+++ projeto-sistema-biblioteca/app/Http/Controllers/Admin/AlunoController.php
@@ -58,31 +58,7 @@ class AlunoController extends Controller
             'data_nascimento' => $validatedAluno['data_nascimento'],
         ]);
 
-        return redirect()->route('admin.index')->with('success', 'Aluno criado com sucesso!');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
+        return redirect()->route('admin.alunos.index')->with('success', 'Aluno criado com sucesso!');
     }
 
     /**
