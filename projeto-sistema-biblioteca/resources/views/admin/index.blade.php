3c895669deabcfd423e0cb9aa2a3edb612715fd9
nicholaskotzias <nicokotzias@gmail.com>
2025-07-01 19:48:42 -0300

    chore: habilitar botao de 'manter categoria' no index de admin

--- projeto-sistema-biblioteca/resources/views/admin/index.blade.php
+++ projeto-sistema-biblioteca/resources/views/admin/index.blade.php
@@ -46,7 +46,7 @@
                 Manter Exemplar
             </a>
 
-            <a href="#" class="flex items-center justify-center gap-3 py-3 rounded-xl bg-gradient-to-r from-teal-600 to-cyan-700 text-white font-semibold shadow-lg hover:from-teal-700 hover:to-cyan-800 transition w-full max-w-full">
+            <a href="admin.categorias.index" class="flex items-center justify-center gap-3 py-3 rounded-xl bg-gradient-to-r from-teal-600 to-cyan-700 text-white font-semibold shadow-lg hover:from-teal-700 hover:to-cyan-800 transition w-full max-w-full">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 10h16M4 14h16M4 18h16" />
                 </svg>
