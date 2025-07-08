fc897d6b03778c1337795b548fe15297d5bdcb5e
nicholaskotzias <nicokotzias@gmail.com>
2025-07-07 21:25:03 -0300

    feat: reorganiza rotas e adiciona controle de empréstimos e exemplares
    - Refatora rotas com prefixos e nomes agrupados para 'admin' e 'alunos'
    - Adiciona rotas para gerenciamento de exemplares no admin
    - Implementa rotas para aprovação, recusa e listagem de empréstimos pendentes no admin
    - Cria rotas para alunos visualizarem, solicitarem e devolverem empréstimos
    - Mantém rotas de perfil e autenticação organizadas

--- projeto-sistema-biblioteca/routes/web.php
+++ projeto-sistema-biblioteca/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmprestimoController;
 use App\Http\Controllers\AlunoController;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\AlunoMiddleware;
@@ -9,6 +10,7 @@ use App\Http\Controllers\Admin\AlunoController as AdminAlunoController;
 use App\Http\Controllers\Admin\CategoriaController as AdminCategoriaController;
 use App\Http\Controllers\Admin\AutorController as AdminAutorController;
 use App\Http\Controllers\Admin\LivroController as AdminLivroController;
+use App\Http\Controllers\Admin\ExemplarController as AdminExemplarController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,17 +20,35 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware(['auth', AdminMiddleware::class])->group(function () {
-    Route::get('/admin', function () {
-        return view('admin.index');
-    })->name('admin.index');
-    Route::get('/admin/alunos', [AdminAlunoController::class, 'index'])->name('admin.alunos.index');
-    Route::get('/admin/alunos/create', [AdminAlunoController::class, 'create'])->name('admin.alunos.create');
-    Route::post('/alunos', [AdminAlunoController::class, 'store'])->name('admin.alunos.store');
-    Route::delete('/alunos/{id}', [AdminAlunoController::class, 'destroy'])->name('admin.alunos.destroy');
-    Route::resource('/admin/categorias', AdminCategoriaController::class)->names('admin.categorias');
-    Route::resource('/admin/autores', AdminAutorController::class)->names('admin.autores');
-    Route::resource('/admin/livros', AdminLivroController::class)->names('admin.livros');
+Route::middleware(['auth', AdminMiddleware::class])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', fn () => view('admin.index'))->name('index');
+
+    Route::get('/alunos', [AdminAlunoController::class, 'index'])->name('alunos.index');
+    Route::get('/alunos/create', [AdminAlunoController::class, 'create'])->name('alunos.create');
+    Route::post('/alunos', [AdminAlunoController::class, 'store'])->name('alunos.store');
+    Route::delete('/alunos/{id}', [AdminAlunoController::class, 'destroy'])->name('alunos.destroy');
+
+    Route::resource('/categorias', AdminCategoriaController::class);
+    Route::resource('/autores', AdminAutorController::class);
+    Route::resource('/livros', AdminLivroController::class);
+    Route::resource('/exemplares', AdminExemplarController::class);
+
+    Route::get('/emprestimos/pendentes', [EmprestimoController::class, 'indexAdmin'])->name('emprestimos.pendentes');
+    Route::post('/emprestimos/aprovar/{id}', [EmprestimoController::class, 'aprovar'])->name('emprestimos.aprovar');
+    Route::delete('/emprestimos/recusar/{id}', [EmprestimoController::class, 'recusar'])->name('emprestimos.recusar');
+});
+
+Route::middleware(['auth', AlunoMiddleware::class])->prefix('alunos')->name('alunos.')->group(function () {
+    Route::get('/', [AlunoController::class, 'index'])->name('index');
+
+    Route::get('/emprestimos', [EmprestimoController::class, 'indexAluno'])->name('emprestimos.index');
+    Route::get('/emprestimos/solicitar', [EmprestimoController::class, 'formSolicitacao'])->name('emprestimos.form');
+    Route::post('/emprestimos/solicitar/{exemplarId}', [EmprestimoController::class, 'solicitar'])->name('emprestimos.solicitar');
+    Route::post('/emprestimos/devolver/{id}', [EmprestimoController::class, 'devolver'])->name('emprestimos.devolver');
+
+    Route::get('/{id}', [AlunoController::class, 'show'])->name('show');
+    Route::get('/{id}/edit', [AlunoController::class, 'edit'])->name('edit');
+    Route::put('/{id}', [AlunoController::class, 'update'])->name('update');
 });
 
 Route::middleware('auth')->group(function () {
@@ -36,13 +56,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-Route::middleware(['auth', AlunoMiddleware::class])->group(function () {
-    Route::get('/alunos', [AlunoController::class, 'index'])->name('alunos.index');
-});
-
-Route::get('/alunos/{id}', [AlunoController::class, 'show'])->name('alunos.show');
-Route::get('/alunos/{id}/edit', [AlunoController::class, 'edit'])->name('alunos.edit');
-Route::put('/alunos/{id}', [AlunoController::class, 'update'])->name('alunos.update');
 
 require __DIR__.'/auth.php';
